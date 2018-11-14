d9d5611848216563080de1ca674c0f1491ed7cc1
lorenzoconcas <35010999+lorenzoconcas@users.noreply.github.com>
2018-11-14 19:38:46 +0100

    - Aggiunti i proiettili ( non multithreaded quindi quando si spara non si può muovere il player);

--- Invaders/Invaders/Libs.h
+++ Invaders/Invaders/Libs.h
@@ -9,3 +9,4 @@
 //Definizioni
 #define TIME_SECOND 1000000 //1 sec = 1mln ms
 #define SLEEP_TIME 1*TIME_SECOND  //da cambiare in futuro
+#define BULLETS_SPEED TIME_SECOND/8 //un ottavo di secondo = 125ms
--- Invaders/Invaders/main.c
+++ Invaders/Invaders/main.c
@@ -19,11 +19,33 @@ void playerThread() {
 			posX++;
 			break;
 		}
+		case ' ': {
+			animateFire(posX, posY);
+			break;
+		}
 		}
 
 		printPlayer(posX, posY);
 	};
 }
+void animateFire(int x, int y) {
+	int xLeft = x-1;
+	int xRight = x + 5;
+	y--;
+
+
+	for (; xLeft > 0, y > 0, xRight < getmaxx(stdscr); xLeft--, y--, xRight++) {
+		mvaddch(y, xLeft, '$');
+		mvaddch(y + 1, xLeft + 1, ' ');
+		mvaddch(y, xRight, '$');
+		mvaddch(y + 1, xRight - 1, ' ');
+		//mvprintw(1, 1, "%d %d", x, y);
+		usleep(BULLETS_SPEED);
+		refresh();
+	}
+	mvprintw(0, 0, "ho finito");
+}
+
 int main()
 {
 	//variabili
